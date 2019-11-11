546019294a8f1320549a37e214b490a73f0459ca
whosAle <abelgrave95@gmail.com>
2019-11-10 21:38:44 -0500

    create class and run method

---
+++ reading_list.rb
@@ -0,0 +1,11 @@
+class ReadingList
+  def initialize
+    # array to hold all of the users saved books
+    @reading_list = [] 
+  end
+
+  def run_app
+    puts "app is running"        
+  end
+
+end
