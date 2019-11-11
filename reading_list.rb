837369e439383593885d98a85e3fafdcbad698fb
whosAle <abelgrave95@gmail.com>
2019-11-10 21:53:38 -0500

    comments

--- reading_list.rb
+++ reading_list.rb
@@ -12,10 +12,12 @@ class ReadingList
 
   end
 
-  def view_add_exit
+  def view_add_exit    
+    #top level menu that gives the user the option to add, view, or leave the app
     puts "Please Enter: \n1 to add to library \n2 to view your Reading List\n3 or to Exit"
     input = gets.chomp.to_i
 
+    #a validation to ensure correct input
     while !(1..3).include?(input)
       puts "Please enter a valid number"
       input = gets.chomp.to_i
