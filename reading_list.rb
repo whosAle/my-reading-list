6d64df9d84dfea4aaebab4b3e841b1658af11378
whosAle <abelgrave95@gmail.com>
2019-11-10 21:52:13 -0500

    top level menu correctly gets input

--- reading_list.rb
+++ reading_list.rb
@@ -5,7 +5,23 @@ class ReadingList
   end
 
   def run_app
-    puts "app is running"        
+    # puts "app is running"  
+    puts "Welcome to MyReadingList powered by Google Books"
+    
+    mode = view_add_exit
+
+  end
+
+  def view_add_exit
+    puts "Please Enter: \n1 to add to library \n2 to view your Reading List\n3 or to Exit"
+    input = gets.chomp.to_i
+
+    while !(1..3).include?(input)
+      puts "Please enter a valid number"
+      input = gets.chomp.to_i
+    end
+
+    input   
   end
 
 end
